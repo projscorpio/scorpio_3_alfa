b403d21efe2e85b621e6a4e92310362e4a9aef75
Artur Bauer <bauerart1@gmail.com>
2018-11-13 15:32:26 +0100

    Switched server and client in video streaming
    switched server and client in video streaming. Required testing, changed
    server type to udp.  Hope it can work with udp. Added server ip variable
    in bash scrypt.

--- pc_code/video_streaming/video_start_streaming_receiver.sh
+++ pc_code/video_streaming/video_start_streaming_receiver.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #Video streaming reveiver script
-netcat -l -p 6000 | mplayer -fps 60 -cache 2048 -
+
+srv_port="192.168.1.101"
+#netcat -l -p 6000 | mplayer -fps 60 -cache 2048 -
+netcat -u $srv_port 6000 | mplayer -fps 60 -cache 2048 -
--- rpi_code/video_streaming/video_start_streaming.sh
+++ rpi_code/video_streaming/video_start_streaming.sh
@@ -1,3 +1,5 @@
 #!/bin/bash 
 #Camera sending script
-raspivid -t 0 -w 1920 -h 1080 -o - | nc 192.168.1.111 6000
+
+#raspivid -t 0 -w 1920 -h 1080 -o - | nc 192.168.1.111 6000
+raspivid -t 0 -w 1920 -h 1080 -o - | netcat -l -u 6000 
