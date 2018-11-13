e85f39c1950dc8432320092b2f535d97e39eaf3c
Artur Bauer <bauerart1@gmail.com>
2018-11-13 15:55:21 +0100

    Added flag -q 1 on server

--- rpi_code/video_streaming/video_start_streaming.sh
+++ rpi_code/video_streaming/video_start_streaming.sh
@@ -2,4 +2,4 @@
 #Camera sending script
 
 #raspivid -t 0 -w 1920 -h 1080 -o - | nc 192.168.1.111 6000
-raspivid -t 0 -w 1920 -h 1080 -o - | netcat -l -u 6000 
+raspivid -t 0 -w 1920 -h 1080 -o - | netcat -l -u -p 6000  -q 1
