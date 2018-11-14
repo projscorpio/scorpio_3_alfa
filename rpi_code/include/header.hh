767d4bb5a4e01568743509ad132662681340a4b9
Artur Bauer <bauerart1@gmail.com>
2018-11-14 08:23:27 +0100

    Added logs on wheels, edited Makefile in pc_code

--- rpi_code/include/header.hh
+++ rpi_code/include/header.hh
@@ -99,6 +99,7 @@ void controllerPositions::right_wheel(bool is_forward, int duty_cycle){
 		gpioWrite(INB_R,LOW);
 	}
 	gpioPWM(PWM_R,duty_cycle);
+  printf("[Controller Positions][Right Wheel] %s: %d", is_forward? "forward": "reverse", turn);
 }
 
 void controllerPositions::left_wheel(bool is_forward, int duty_cycle){
@@ -110,5 +111,6 @@ void controllerPositions::left_wheel(bool is_forward, int duty_cycle){
 		gpioWrite(INB_L,HIGH);
 	}
 	gpioPWM(PWM_L,duty_cycle);
+  printf("[Controller Positions][Left Wheel] %s: %d", is_forward? "forward": "reverse", turn);
 }
 #endif
