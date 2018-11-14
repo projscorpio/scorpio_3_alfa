39abfaed6173aa6e8cdc192c198fd79049e86180
Artur Bauer <bauerart1@gmail.com>
2018-11-14 07:24:13 +0100

    Changed server type to tcp, added recive timeout

--- pc_code/video_streaming/video_start_streaming_receiver.sh
+++ pc_code/video_streaming/video_start_streaming_receiver.sh
@@ -2,5 +2,4 @@
 #Video streaming reveiver script
 
 srv_port="192.168.1.101"
-#netcat -l -p 6000 | mplayer -fps 60 -cache 2048 -
-netcat -u $srv_port -p 6000 | mplayer -fps 60 -cache 2048 -
+netcat  $srv_port  6000 -w 1| mplayer -fps 60 -cache 2048 -
--- rpi_code/video_streaming/video_start_streaming.sh
+++ rpi_code/video_streaming/video_start_streaming.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
 #Camera sending script
 
-#raspivid -t 0 -w 1920 -h 1080 -o - | nc 192.168.1.111 6000
-raspivid -t 0 -w 1920 -h 1080 -o - | netcat -l -u -p 6000  -q 1
+raspivid -t 0 -w 1920 -h 1080 -o - | netcat -l -p 6000  -q 1
